d30ad2511346afcc14b343f0b4c48aa48faa7c2f
Dave Arnold <darnold@hashicorp.com>
2018-06-21 21:48:14 -0700

    adding nat_gateway output

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,7 @@ output "admin_sg" {
   value       = "${aws_security_group.admin.id}"
   description = "This is a security group that is within the private subnet. Any instance that is assigned this security group will allow ssh traffic from any other instance that is assigned this security group"
 }
+
+output "nat_gateway" {
+  value = "${aws_nat_gateway.nat_gw.id}"
+}
