5b6ef99d2369a4e57017bca09c9eb6b976e0f6d5
Dave Arnold <dave.arnold@harness.io>
2019-09-16 13:02:07 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,5 +3,5 @@ resource "aws_subnet" "subnet" {
   cidr_block              = "${var.subnet_cidr}"
   map_public_ip_on_launch = false
   availability_zone       = "${var.availability_zone}"
-  tags                    = "${merge(map("Name", "${var.network_name}-${var.availability_zone}-public"), var.resource_tags)}"
+  tags                    = "${merge(map("Name", "${var.network_name}-${var.availability_zone}-private"), var.resource_tags)}"
 }
